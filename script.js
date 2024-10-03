5357af288195794fd287d739e701d7397c8d5f0d
Bounbaze_MAMAH <mamah232@gmail.com>
2024-10-03 14:22:25 +0000

    system to-Dolist sans system de sauvegarde

--- script.js
+++ script.js
@@ -2,14 +2,49 @@ const jsInput = document.querySelector('.js-iput');
 const jsBtn = document.querySelector('.js-btn');
 const jsList = document.querySelector('.js-list')
 function addList(){
-  jsList.innerHTML += `<li class=" js-element">${jsInput.value} <span>&#215;</span></li>`;
-  const jsElement = document.querySelectorAll('.js-element');
-  console.log(jsElement)
+
+  //ne pas enregistrer une ligne vide
+  if(jsInput.value.trim()===''){
+    return;
+  }
+
+  //create li
+  const newList = document.createElement('li');
+  newList.innerHTML = `${jsInput.value} <span class="js-span">&#215;</span>`
+  jsList.appendChild(newList);
+  const jsSpan = newList.querySelector('.js-span');
+
+  //remove list by click on close button
+  jsSpan.addEventListener('click', ()=>{
+    newList.remove()
+  })
+
+  //line-through on do list
+  newList.addEventListener('click', ()=>{
+    if(newList.className !== 'select'){
+      newList.classList.add('select')
+    }else{
+      newList.classList.remove('select')
+    }
+  })
+  
 }
+
+// save systeme
+function saveData(){
+  localStorage.setItem('data', jsList.innerHTML)
+}
+function showTask(){
+  jsList.innerHTML = localStorage.getItem('data')
+}
+
+
 jsBtn.addEventListener('click', ()=>{
   addList();
   jsInput.value = '';
 })
+
+//valid with enter on keybord
 function pressKeybord (event){
   if(event.key === 'Enter'){
     addList();
@@ -17,3 +52,5 @@ function pressKeybord (event){
   }
 }
 jsInput.addEventListener('keydown', pressKeybord)
+
+
